a63d22a2e8e8abbb7b0b62fb8df908b6f46fdb67
Peter Chen <tsehsun@Peter-Chens-MacBook-Pro.local>
2012-12-06 21:23:55 -0500

    remove print when a file doesn't have any commit

--- retrieveGitSourceCode.py
+++ retrieveGitSourceCode.py
@@ -298,10 +298,11 @@ def getIssueKeyInfo(fileInfoMap, rootDir):
 				sys.stderr.write("no such file: "+ fileName+"\n")
 			"""
 		except IndexError, e:
-			print "get bug count index error, no such file: ", fileName
-			exc_type, exc_obj, exc_tb = sys.exc_info()
-			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]      
-			print(exc_type, fname, exc_tb.tb_lineno)
+			#print "get bug count index error, no such file: ", fileName
+			#exc_type, exc_obj, exc_tb = sys.exc_info()
+			#fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]      
+			#print(exc_type, fname, exc_tb.tb_lineno)
+			pass
 	return issueKeyInfo#bugdataOld 
 	
 
